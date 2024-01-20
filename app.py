9574dc3229b97c16c4b23e06837c540570f3efab
Mike1ife <yitai77@gmail.com>
2024-01-20 17:46:05 +0800

    test

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def handle_message(event):
                     ],
                 ),
                 CarouselColumn(
-                    thumbnail_image_url="https://raw.githubusercontent.com/Mike1ife/Camel-Up/main/images/Purple5.png",
+                    thumbnail_image_url="https://github.com/Mike1ife/Camel-Up/blob/main/images/Purple5.png?raw=true",
                     title="紫色駱駝",
                     text="紫色駱駝賭塊",
                     actions=[
